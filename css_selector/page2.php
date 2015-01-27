393dfdda8810fec0555e47c4411052635eb4f8bf
Muktaranibiswas <muktaquate@yahoo.com>
2015-01-27 12:44:00 +0600

    css,jquery uses website

---
+++ css_selector/page2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Block1</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<header>
+
+</header>
+
+<section>
+    <div class="collection">
+        <article class="story theme-summary">
+            <h3 class="kicker">White House Memo </h3>
+            <h2 class="story-heading">
+                <a href="http://www.nytimes.com/2015/01/22/us/politics/gloom-lifts-and-obama-goes-all-out.html">Gloom Lifts, and Obama Goes All Out</a>
+                <span class="product-label theme-nyt-now ">
+                    <span class="visually-hidden">NYT Now</span>
+                    <i class="icon dot-logo-icon"></i>
+                </span>
+            </h2>
+
+            <div class="thumb">
+                <a href="http://www.nytimes.com/2015/01/22/us/politics/gloom-lifts-and-obama-goes-all-out.html">
+                    <img alt="" src="http://static01.nyt.com/images/2015/01/20/multimedia/sotu-obama-zinger/sotu-obama-zinger-thumbStandard-v3.jpg">
+                </a>
+            </div>
+
+            <p class="byline">By MICHAEL D. SHEAR
+                <time class="timestamp">9:43 PM ET</time>
+            </p>
+
+            <p class="summary">
+                President Obama’s self-assured State of the Union address reflected his sense of being liberated and emboldened, White House aides said.
+            </p>
+        </article>
+    </div>
+</section>
+
+<footer>
+
+</footer>
+</body>
+</html>
---
+++ css_selector/test1.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+.column {
+    width: 20%;
+    float: left;
+}
+.theme-summary {
+    margin-bottom: 12px;
+}
+.wide-thumb {
+    position: relative;
+	width: 20%;
+}
+.summary {
+    font-size: 0.8125rem;
+    line-height: 1rem;
+    font-weight: 400;
+    font-family: georgia,"times new roman",times,serif;
+    padding-top: 3px;
+    color: #666;
+}
+.story-heading {
+    font-size: 1rem;
+    line-height: 1.25rem;
+    font-weight: 700;
+    font-family: "nyt-cheltenham",georgia,"times new roman",times,serif;
+    padding-top: 10px;
+}
+.span-ab-layout .c-column {
+    width: 75px;
+}
+.column {
+    width: 177px;
+    float: left;
+}
+a:link, a:visited {
+    color: #000;
+}
+a:link {
+    text-decoration: none;
+    color: #326891;
+}
+</style>
+</head>
+<body>
+
+<h2 class="section-heading">
+</h2>
+<div class="column">
+    <article class="story theme-summary " data-story-id="100000003464569" data-rank="0" data-collection-renderstyle="HPMediumMediaHedSumDaypart">
+        <a href="http://www.nytimes.com/2015/01/22/sports/facing-76ers-knicks-earn-a-win-someone-had-to.html">
+            <div class="wide-thumb">
+                <img src="http://static01.nyt.com/images/2015/01/22/sports/CITY-KNICKS/CITY-KNICKS-mediumThreeByTwo210.jpg">
+            </div>
+        </a>
+        <h2 class="story-heading">
+            <a href="http://www.nytimes.com/2015/01/22/sports/facing-76ers-knicks-earn-a-win-someone-had-to.html"> Facing 76ers, Knicks Earn a Win. Someone Had To. </a>
+            <span class="product-label theme-nyt-now hidden"><span class="visually-hidden">NYT Now</span><i class="icon dot-logo-icon"></i></span>        </h2>
+        <p class="summary">
+            In a game between two teams jockeying for the top pick in the N.B.A. draft, the Knicks, whether they liked it or not, took home the victory.        </p>
+    </article>
+</div>
+</div>
+
+
+</body>
+</html>
